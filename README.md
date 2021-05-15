98f1a15fed82f9e8b231b0ddd7dfed61f7bba532
guille <guillermo.lopez@coderio.co>
2021-05-15 20:38:19 -0300

    firt commit and method to create changelog file

---
+++ README.md
@@ -0,0 +1,5 @@
+# changelog_creator
+
+Dart script para generar el archivo CHANGELOG.md
+
+para ejecutar hacer: flutter pub run changelog_creator:main
---
+++ bin/main.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+final String currentDir = Directory.current.path;
+final changelogFile = File('$currentDir/CHANGELOG.md');
+
+void main(List<String> arguments) async{
+
+  stdout.writeln('--BIENVENIDO A CHANGELOG CREATOR--');
+  createFileChangeLog();
+}
+
+///crea el archivo CHANGELOG si no existe
+void createFileChangeLog() async{
+  stdout.writeln('---Verificando archivo CHANGELOG.md---');
+  if(! await changelogFile.exists()){
+    try{
+      changelogFile.create();
+      stdout.writeln('Archivo CHANGELOG.md creado');
+      await changelogFile.writeAsString('# Changelog');
+    }catch(_){
+      stdout.writeln('Error al crear el archivo CHANGELOG.md');
+    }
+  }
+}
