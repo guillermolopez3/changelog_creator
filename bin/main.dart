c1329508765323ca7d4112279d17ec2e11f9701d
guille <guillermo.lopez@coderio.co>
2021-05-16 16:24:30 -0300

    modifica la version del pubspec

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,23 @@
+import 'dart:convert';
 import 'dart:io';
+
+
 
 final String currentDir = Directory.current.path;
 final changelogFile = File('$currentDir/CHANGELOG.md');
+final List<String> version = [];
 
 void main(List<String> arguments) async{
 
-  stdout.writeln('--BIENVENIDO A CHANGELOG CREATOR!--');
-  createFileChangeLog();
+  exitCode = 0;
+  /*final parser = ArgParser()
+    ..addOption('repo', abbr: 'r', defaultsTo: 'gitlab');//gilab o github
+  final argResults = parser.parse(arguments);*/
+
+  stdout.writeln('--BIENVENIDO A CHANGELOG CREATOR--');
+  //createFileChangeLog();
+  //readChange();
+  setVersionPubspec();
 }
 
 ///crea el archivo CHANGELOG si no existe
@@ -23,3 +34,32 @@ void createFileChangeLog() async{
   }
 }
 
+void readChange() async{
+  RegExp reLineVersion = RegExp(r'^\#\# ([0-9]\d*)\.(\d+)\.(\d+)'); //formato valido: ## 0.0.1
+  RegExp reExtractVersion = RegExp(r'([0-9]\d*)\.(\d+)\.(\d+)');
+  await changelogFile
+      .openRead()
+      .map(utf8.decode)
+      .transform(new LineSplitter())
+      .forEach((l){
+        if(reLineVersion.hasMatch(l)){
+          version.add(reExtractVersion.stringMatch(l));
+          //stdout.writeln('linea: $l');
+        }
+  });
+
+  version.forEach((element)=> stdout.writeln('V: $element') );
+
+}
+
+//modifico el versionado del pubspec.yaml
+void setVersionPubspec(){
+  final File yaml = File('$currentDir/pubspec.yaml');
+  if(!yaml.existsSync()) {
+    stdout.writeln('no existe el archivo yaml');
+    return;
+  }
+  String content = yaml.readAsStringSync();
+  content = content.replaceFirst(new RegExp(r"version: .*"),"version: 0.2.0");
+  yaml.writeAsStringSync(content);
+}
